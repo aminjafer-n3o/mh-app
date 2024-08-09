7f05ba53c02cb65ab2b95e42f41b21e94fe4f5a1
aminjafer-n3o <amin.jafer@n3o.ltd>
2024-08-09 15:40:26 +0500

    changed-the-names-for-custom-variables

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: process.env.APP_ID ? process.env.APP_ID : 'io.ionic.starter',
-  appName: process.env.APP_NAME ? process.env.APP_NAME : 'Muslim Hands',
+  appId: process.env.CUSTOM_APP_ID ? process.env.CUSTOM_APP_ID : 'io.ionic.starter',
+  appName: process.env.CUSTOM_APP_NAME ? process.env.CUSTOM_APP_NAME : 'Muslim Hands',
   webDir: 'dist'
 };
 
