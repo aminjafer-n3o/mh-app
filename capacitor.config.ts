7b6c19d705b3e48000357e311089b59035c747fd
aminjafer-n3o <amin.jafer@n3o.ltd>
2024-08-09 15:37:27 +0500

    changes-to-test-appflow-variables

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Muslim Hands',
+  appId: process.env.APP_ID ? process.env.APP_ID : 'io.ionic.starter',
+  appName: process.env.APP_NAME ? process.env.APP_NAME : 'Muslim Hands',
   webDir: 'dist'
 };
 
